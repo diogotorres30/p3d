a3292b0ffed1b256f20956506598db2a8efad7ae
DESKTOP-5SNT74G\torres <diogo.torres.30@gmail.com>
2018-03-02 18:34:48 +0000

    nff

--- RayTracing/TurnerRayTracing/src/NFFLoader.cpp
+++ RayTracing/TurnerRayTracing/src/NFFLoader.cpp
@@ -1,1 +1,17 @@
 #include "NFFLoader.h"
+
+void NFFLoader::parseLine(std::stringstream& sin)
+{
+	std::string s;
+	sin >> s;
+	if (s.compare("v") == 0) parseVertex(sin);
+	else if (s.compare("vt") == 0) parseTexcoord(sin);
+	else if (s.compare("vn") == 0) parseNormal(sin);
+	else if (s.compare("usemtl") == 0) parseMesh(sin);
+	//else if (s.compare("s") == 0) {
+	//	sin >> s;
+	//	if (s.compare("1") == 0) smoothedFace = true;
+	//	else smoothedFace = false;
+	//}
+	else if (s.compare("f") == 0 /*&& smoothedFace*/) parseFace(sin);
+}
