f823265b2ff08ded8e31f59f5b02af5f527d65e1
Baltazar95 <tiago_mpb95@hotmail.com>
2018-04-17 11:18:58 +0100

    [UPDATE] removed unused line

--- RayTracing/TurnerRayTracing/src/Ray.cpp
+++ RayTracing/TurnerRayTracing/src/Ray.cpp
@@ -12,7 +12,6 @@ Ray::Ray(Camera *camera, Vector2 pt)
 	Vector3 x = camera->getWidth() * ((pt.x / camera->getResX()) - 0.5f) * camera->getXe();
 
 	direction = normalized(z + y + x);
-	//direction = normalized(Vector3(pt, 0.0f) - camera->getFrom());
 	scalar = 0.0f;
 }
 
